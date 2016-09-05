196310d3cf26a0f5684f7c82bf6f40f11cd43571
Guillaume Gourdin <ggourdin@dxo.com>
2016-09-05 18:29:00 +0200

    Added move operator on image_buffer_abc.

--- ggo2d/src/image/ggo_image_buffer_abc.h
+++ ggo2d/src/image/ggo_image_buffer_abc.h
@@ -13,11 +13,18 @@ namespace ggo
   {
   public:
 
-                           image_buffer_abc(int width, int height, data_t * buffer, bool delete_buffer);
-    virtual               ~image_buffer_abc();
+                            image_buffer_abc(image_buffer_abc<color_t, data_t> && rhs);
+                            image_buffer_abc(int width, int height, data_t * buffer, bool delete_buffer);
+    virtual                ~image_buffer_abc();
 
-            const data_t * data() const { return _buffer; }
-            data_t *       data() { return _buffer; }
+            const data_t *  data() const { return _buffer; }
+            data_t *        data() { return _buffer; }
+
+            void            operator=(image_buffer_abc<color_t, data_t> && rhs);
+
+  // No copy.
+                            image_buffer_abc(const image_buffer_abc<color_t, data_t> & rhs) = delete;
+            void            operator=(const image_buffer_abc<color_t, data_t> & rhs) = delete;
 
   protected:
     
@@ -28,6 +35,19 @@ namespace ggo
 
 namespace ggo
 {
+  /////////////////////////////////////////////////////////////////////
+  template <typename color_t, typename data_t>
+  image_buffer_abc<color_t, data_t>::image_buffer_abc(image_buffer_abc<color_t, data_t> && rhs)
+  :
+  image_abc<color_t>(width, height),
+  _buffer(rhs._buffer),
+  _delete_buffer(rhs._delete_buffer)
+  {
+    rhs._buffer = nullptr;
+    rhs._delete_buffer = false;
+  }
+
+
   /////////////////////////////////////////////////////////////////////
   template <typename color_t, typename data_t>
   image_buffer_abc<color_t, data_t>::image_buffer_abc(int width, int height, data_t * buffer, bool delete_buffer)
@@ -46,6 +66,24 @@ namespace ggo
     {
       delete[] _buffer;
     }
+  }
+
+  /////////////////////////////////////////////////////////////////////
+  template <typename color_t, typename data_t>
+  void image_buffer_abc<color_t, data_t>::operator=(image_buffer_abc<color_t, data_t> && rhs)
+  {
+    if (_delete_buffer == true)
+    {
+      delete[] _buffer;
+    }
+
+    _width = rhs._width;
+    _height = rhs._height;
+    _buffer = rhs._buffer;
+    _delete_buffer = rhs._delete_buffer;
+
+    rhs._buffer = nullptr;
+    rhs._delete_buffer = false;
   }
 }
 
