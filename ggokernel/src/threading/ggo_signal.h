bf78df21d162b3c433c44e593ccece2c514f373f
Guillaume Gourdin <ggourdin@dxo.com>
2016-11-16 15:41:44 +0100

    Created signal class.

---
+++ ggokernel/src/threading/ggo_signal.h
@@ -0,0 +1,31 @@
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+namespace ggo
+{
+  class signal
+  {
+  public:
+
+    void wait()
+    {
+      std::unique_lock<std::mutex> lock(_mutex);
+      _wait_notification.wait(lock, [&] { return _notified; });
+      _notified = false;
+    }
+
+    void notify()
+    {
+      std::unique_lock<std::mutex> lock(_mutex);
+      _notified = true;
+      _wait_notification.notify_one();
+    }
+
+  private:
+
+    bool                    _notified = false;
+    std::mutex              _mutex;
+    std::condition_variable _wait_notification;
+  };
+}
