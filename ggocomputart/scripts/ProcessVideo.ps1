796fc4844d95e255b25990eb4f9c7e0b74dcdbc2
Guillaume Gourdin <ggourdin@dxo.com>
2018-02-26 16:46:41 +0100

    Switched to GraphicsMagick.

--- ggocomputart/scripts/ProcessVideo.ps1
+++ ggocomputart/scripts/ProcessVideo.ps1
@@ -5,21 +5,19 @@ ffmpeg -i $InputFile _%05d.tga
 
 Write-Host " Creating thunmbnail ... " -ForegroundColor DarkBlue -BackgroundColor White
 $OutputJpg = $OutputBasename + ".jpg"
-convert _00050.tga -resize 1066x600! -crop 600x600+223+0 +repage -page +265+260 $Play -flatten $OutputJpg
+gm.exe convert -resize 1066x600! -crop 600x600+223+0 _00050.tga -page +265+260 $Play -flatten $OutputJpg
 
 Write-Host " Adding logo ... " -ForegroundColor DarkBlue -BackgroundColor White
 Get-ChildItem _*.tga | ForEach-Object { `
-	convert.exe $_ -resize 640x360! -page +10+10 $Logo -flatten -flip tmp.tga; `
+  Write-Host -NoNewline "."; `
+	gm.exe convert -resize 640x360! $_ -page +10+10 $Logo -flatten tmp.tga; `
   Remove-Item $_; `
   Move-Item tmp.tga $_; `
 }
+Write-Host ""
 
 Write-Host " Creating mp4 file ... " -ForegroundColor DarkBlue -BackgroundColor White
 $OutputMp4 = $OutputBasename + ".mp4"
-ffmpeg -i "_%05d.tga" -r 25 -c:v libx264 -q:v 6 -y $OutputMp4
-
-Write-Host " Creating ogg file ... " -ForegroundColor DarkBlue -BackgroundColor White
-$OutputOgg = $OutputBasename + ".ogg"
-ffmpeg -i "_%05d.tga" -r 25 -c:v libtheora -q:v 6 -y $OutputOgg
+ffmpeg -i "_%05d.tga" -r 25 -c:v libx264 -pix_fmt yuv420p -profile:v baseline -crf 20 $OutputMp4
 
 Remove-Item _*.tga
