dc5df915e21a53ee0148706a0b5ede860b554bc4
Guillaume Gourdin <ggourdin@dxo.com>
2018-03-26 16:27:16 +0200

    Added paths non-regression tests.

---
+++ ggoanimation/test/ggo_path_nonreg.cpp
@@ -0,0 +1,31 @@
+#include <ggo_nonreg.h>
+#include <ggo_velocity_path.h>
+#include <ggo_spiral_path.h>
+
+/////////////////////////////////////////////////////////////////////
+GGO_TEST(path, velocity)
+{
+  ggo::velocity_path path(ggo::vec2f(1.f, 2.f));
+
+  const auto p1 = path.get_pos(0);
+  GGO_CHECK_FLOAT_EQ(p1.x(), 0.f);
+  GGO_CHECK_FLOAT_EQ(p1.y(), 0.f);
+
+  const auto p2 = path.get_pos(10);
+  GGO_CHECK_FLOAT_EQ(p2.x(), 10.f);
+  GGO_CHECK_FLOAT_EQ(p2.y(), 20.f);
+}
+
+/////////////////////////////////////////////////////////////////////
+GGO_TEST(path, spiral)
+{
+  ggo::spiral_path path(ggo::pi<float>() / 2, 1.f);
+
+  const auto p1 = path.get_pos(0);
+  GGO_CHECK_FLOAT_EQ(p1.x(), 0.f);
+  GGO_CHECK_FLOAT_EQ(p1.y(), 0.f);
+
+  const auto p2 = path.get_pos(2);
+  GGO_CHECK_FLOAT_EQ(p2.x(), -2.f);
+  GGO_CHECK_FLOAT_EQ(p2.y(), 0.f);
+}
