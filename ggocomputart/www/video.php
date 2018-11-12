bf38cdf3acc169aef8a5f502ef69df9ad2fe1590
Guillaume Gourdin <ggourdin@dxo.com>
2018-11-12 16:49:24 +0100

    Video is not fit and autoplay.

--- ggocomputart/www/video.php
+++ ggocomputart/www/video.php
@@ -5,10 +5,10 @@ $video = $_GET["video"];
 
 echo "<!DOCTYPE html>\n<html>\n<head>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<style>\n";
 echo "body { margin: 0; }\n";
-echo "#myVideo { position: fixed; right: 0; bottom: 0; min-width: 100%; min-height: 100%; }\n";
-echo "</style>\n</head>\n<body>\n";
+echo "#myVideo { position:absolute; top:0; left:0; width:100%; height:100%; }\n";
+echo "</style>\n</head>\n<body style=\"background-color:black;\">\n";
 
-echo "<video autoplay muted id=\"myVideo\">\n";
+echo "<video autoplay muted loop id=\"myVideo\">\n";
 echo "  <source src=\"../artists/$artist/video/$video.mp4\" type=\"video/mp4\">\n";
 echo "  Your browser does not support HTML5 video.\n";
 echo "</video>\n";
