a83df1e3ed6af6124991b475069a64136541864c
Guillaume Gourdin <ggourdin@dxo.com>
2018-11-12 16:27:55 +0100

    Made video directly play fullscreen.

--- ggocomputart/www/video.php
+++ ggocomputart/www/video.php
@@ -3,18 +3,15 @@
 $artist = $_GET["artist"];
 $video = $_GET["video"];
 
-include_once("helpers.php");
+echo "<!DOCTYPE html>\n<html>\n<head>\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<style>\n";
+echo "body { margin: 0; }\n";
+echo "#myVideo { position: fixed; right: 0; bottom: 0; min-width: 100%; min-height: 100%; }\n";
+echo "</style>\n</head>\n<body>\n";
 
-html_header($artist, "artists/$artist/$video.jpg");
-
-echo "<body>\n";
-echo "<video controls=\"controls\" poster=\"artists/$artist/video/$video-video-preview.jpg\">\n";
-echo "<source src=\"artists/$artist/video/$video.mp4\" type=\"video/mp4\" />\n";
+echo "<video autoplay muted id=\"myVideo\">\n";
+echo "  <source src=\"../artists/$artist/video/$video.mp4\" type=\"video/mp4\">\n";
+echo "  Your browser does not support HTML5 video.\n";
 echo "</video>\n";
 
-// Footer.
-$url = "http://www.gourdi.net/video.php?artist=$artist&video=$video";
-social_footer($url);
-
-echo "</body></html>\n";
-
+echo "</body>\n";
+echo "</html>\n";
