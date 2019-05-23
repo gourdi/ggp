440d1c4525eac9914e65d98605d40d93b4785de3
Guillaume Gourdin <ggourdin@dxo.com>
2019-05-23 16:52:21 +0200

    Removed unused code.

--- ggokernel/test/sampling/ggo_sampling_nonreg.cpp
+++ ggokernel/test/sampling/ggo_sampling_nonreg.cpp
@@ -42,31 +42,6 @@ GGO_TEST(sampling, disc)
     GGO_CHECK(find_point(samples, { 2, 2 }));
     GGO_CHECK(find_point(samples, { 2, 1 }));
   }
-
-  // Samples count sampling.
-  {
-    ggo::disc_f disc({ 4, 3 }, 1);
-    auto samples = adaptive_grid_sampling(disc, 3, 1000);
-
-    GGO_CHECK(samples.size() == 4);
-
-    GGO_CHECK(find(samples, { 4 + 1 / std::sqrt(2.f), 3 + 1 / std::sqrt(2.f) } ));
-    GGO_CHECK(find(samples, { 4 + 1 / std::sqrt(2.f), 3 - 1 / std::sqrt(2.f) } ));
-    GGO_CHECK(find(samples, { 4 - 1 / std::sqrt(2.f), 3 + 1 / std::sqrt(2.f) } ));
-    GGO_CHECK(find(samples, { 4 - 1 / std::sqrt(2.f), 3 - 1 / std::sqrt(2.f) } ));
-  }
-
-  {
-    ggo::disc_f disc({ -2, 5 }, 7);
-    auto samples = adaptive_grid_sampling(disc, 12345);
-
-    GGO_CHECK(samples.size() == 12352);
-
-    for (const auto & sample : samples)
-    {
-      GGO_CHECK(disc.is_point_inside(sample) == true);
-    }
-  }
 }
 
 /////////////////////////////////////////////////////////////////////
@@ -86,18 +61,6 @@ GGO_TEST(sampling, rect)
     GGO_CHECK(find_point(samples, ggo::pos2_f(3.f, 2.f)));
     GGO_CHECK(find_point(samples, ggo::pos2_f(3.f, 3.f)));
     GGO_CHECK(find_point(samples, ggo::pos2_f(3.f, 4.f)));
-  }
-
-  {
-    ggo::rect_f rect{ 1, 1, 3, 2 };
-
-    auto samples = adaptive_grid_sampling(rect, 3);
-
-    GGO_CHECK(samples.size() == 4);
-    GGO_CHECK(find_point(samples, ggo::pos2_f(1.5f, 1.f)));
-    GGO_CHECK(find_point(samples, ggo::pos2_f(3.5f, 1.f)));
-    GGO_CHECK(find_point(samples, ggo::pos2_f(1.5f, 3.f)));
-    GGO_CHECK(find_point(samples, ggo::pos2_f(3.5f, 3.f)));
   }
 }
 
