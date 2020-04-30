1e80bd16420d3035835499aa744c4f604e6969c5
Guillaume Gourdin <ggourdin@dxo.com>
2020-04-30 09:55:50 +0200

    Improved interpolation by separating data and processing.

--- ggokernel/src/kernel/math/interpolation/ggo_interpolation1d.h
+++ ggokernel/src/kernel/math/interpolation/ggo_interpolation1d.h
@@ -19,7 +19,7 @@ namespace ggo
   }
 }
 
-// Linear sampling.
+// Linear sampling, fixed step.
 namespace ggo
 {
   /////////////////////////////////////////////////////////////////////
@@ -42,7 +42,41 @@ namespace ggo
   }
 }
 
-// Cubic sampling.
+// Linear sampling, variable step.
+namespace ggo
+{
+  /////////////////////////////////////////////////////////////////////
+  template <typename iterator_t, typename scalar_t>
+  auto linear_interpolation1d(iterator_t begin, iterator_t end, scalar_t x)
+  {
+    auto get_prv = [](auto it) { auto prv = it; --prv; return prv; };
+
+    if (std::distance(begin, end) < 2)
+    {
+      throw std::runtime_error("not enough points in the curve");
+    }
+
+    if (x <= begin->first)
+    {
+      return begin->second;
+    }
+
+    for (auto cur = begin; cur != end; ++cur)
+    {
+      if (x <= cur->first)
+      {
+        auto prv = get_prv(cur);
+        auto interpolation = make_interpolation_linear(prv->first, prv->second, cur->first, cur->second);
+
+        return ggo::evaluate(interpolation, x);
+      }
+    }
+
+    return get_prv(end)->second;
+  }
+}
+
+// Cubic sampling, fixed step.
 namespace ggo
 {
   //////////////////////////////////////////////////////////////
@@ -62,6 +96,54 @@ namespace ggo
   data_t cubic_interpolation1d(const data_t * in, int size, scalar_t x)
   {
     return cubic_interpolation1d([&](int i) { return in[ggo::clamp(i, 0, size - 1)]; }, x);
+  }
+}
+
+// Cubic sampling, variable step.
+namespace ggo
+{
+  /////////////////////////////////////////////////////////////////////
+  template <typename iterator_t, typename scalar_t>
+  auto cubic_interpolation1d(iterator_t begin, iterator_t end, scalar_t x)
+  {
+    if (std::distance(begin, end) < 2)
+    {
+      throw std::runtime_error("not enough points in the curve");
+    }
+
+    auto get_prv = [](auto it) { auto prv = it; --prv; return prv; };
+    auto get_nxt = [](auto it) { auto nxt = it; ++nxt; return nxt; };
+
+    if (x <= begin->first)
+    {
+      return begin->second;
+    }
+
+    for (auto cur = get_nxt(begin); cur != end; ++cur)
+    {
+      if (x <= cur->first)
+      {
+        auto prv = get_prv(cur);
+
+        scalar_t x1 = prv->first;
+        scalar_t x2 = cur->first;
+        const auto& y1 = prv->second;
+        const auto& y2 = cur->second;
+
+        scalar_t x0    = prv == begin ? 2 * x1 - x2 : get_prv(prv)->first;
+        const auto& y0 = prv == begin ? y2 : get_prv(prv)->second;
+
+        auto nxt = get_nxt(cur);
+        scalar_t x3    = nxt == end ? 2 * x2 - x1 : nxt->first;
+        const auto& y3 = nxt == end ? y1 : nxt->second;
+
+        auto interpolation = make_interpolation_cubic(x0, y0, x1, y1, x2, y2, x3, y3);
+
+        return ggo::evaluate(interpolation, x);
+      }
+    }
+
+    return get_prv(end)->second;
   }
 }
 
--- ggokernel/test/math/interpolation/ggo_interpolation1d_nonreg.cpp
+++ ggokernel/test/math/interpolation/ggo_interpolation1d_nonreg.cpp
@@ -2,7 +2,7 @@
 #include <kernel/math/interpolation/ggo_interpolation1d.h>
 
 /////////////////////////////////////////////////////////////////////
-GGO_TEST(interpolation1d, linear)
+GGO_TEST(interpolation1d, linear_fixed_step)
 {
   const float data[] = { 1, 2, 7, 5 };
 
@@ -15,7 +15,21 @@ GGO_TEST(interpolation1d, linear)
 }
 
 /////////////////////////////////////////////////////////////////////
-GGO_TEST(interpolation1d, cubic)
+GGO_TEST(interpolation1d, linear_variable_step)
+{
+  const std::map<float, float> data({ { 1.f, 1.f }, { 3.f, 0.f }, { 4.f, 2.f } });
+
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 0.0f), 1.0f);
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 1.0f), 1.0f);
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 2.0f), 0.5f);
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 3.0f), 0.0f);
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 3.5f), 1.0f);
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 4.0f), 2.0f);
+  GGO_CHECK_EQ(ggo::linear_interpolation1d(data.begin(), data.end(), 5.0f), 2.0f);
+}
+
+/////////////////////////////////////////////////////////////////////
+GGO_TEST(interpolation1d, cubic_fixed_step)
 {
   const float data[] = { 1, 2, 7, 5 };
 
@@ -26,3 +40,36 @@ GGO_TEST(interpolation1d, cubic)
   GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data, 4, 0.5f), 1.1875f);
   GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data, 4, 2.5f), 6.3125f);
 }
+
+/////////////////////////////////////////////////////////////////////
+GGO_TEST(interpolation1d, cubic_variable_step)
+{
+  const std::vector<std::pair<float, float>> data({ { 1.f, -1.f }, { 2.f, 3.f }, { 3.f, 2.f } });
+
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 0.9f), -1.000000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.0f), -1.000000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.1f), -0.901501f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.2f), -0.632004f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.3f), -0.230499f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.4f), 0.264000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.5f), 0.812500f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.6f), 1.376000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.7f), 1.915500f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.8f), 2.392000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 1.9f), 2.766500f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.0f), 3.000000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.1f), 3.093510f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.2f), 3.088000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.3f), 3.004520f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.4f), 2.863980f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.5f), 2.687500f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.6f), 2.496000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.7f), 2.310500f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.8f), 2.152010f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 2.9f), 2.041490f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 3.0f), 2.000000f);
+  GGO_CHECK_FLOAT_EQ(ggo::cubic_interpolation1d(data.begin(), data.end(), 3.1f), 2.000000f);
+}
+
+
+
