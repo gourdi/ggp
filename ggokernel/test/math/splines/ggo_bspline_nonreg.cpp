f2e15efe8c9e2997c822c172dd2ad22f8d537e65
Guillaume Gourdin <ggourdin@dxo.com>
2020-06-17 10:07:57 +0200

    bspline

--- ggokernel/test/math/splines/ggo_bspline_nonreg.cpp
+++ ggokernel/test/math/splines/ggo_bspline_nonreg.cpp
@@ -6,7 +6,7 @@
 namespace ggo
 {
   template <typename data_t, typename scalar_t>
-  data_t bspline(std::vector<data_t> control_points, std::vector<scalar_t> knots, scalar_t t)
+  data_t bspline_base(std::vector<data_t> control_points, std::vector<scalar_t> knots, scalar_t t)
   {
     GGO_ASSERT_EQ(2 * (control_points.size() - 1), knots.size());
 
@@ -47,12 +47,22 @@ namespace ggo
   {
     GGO_ASSERT_EQ(knots.size(), control_points.size() + degree - 1);
 
-    int begin = degree - 1;
-    int end   = knots.size() - degree + 1;
-    for (int i = begin; i < end; ++i)
+    size_t begin  = degree - 1;
+    size_t end    = knots.size() - degree + 1;
+    size_t offset = 0;
+    for (size_t i = begin; i < end; ++i)
     {
-      if ()
+      if (t >= knots[i] && t <= knots[i + 1])
+      {
+        break;
+      }
+      ++offset;
     }
+
+    std::vector<data_t> sub_control_points(control_points.begin() + offset, control_points.begin() + offset + degree + 1);
+    std::vector<scalar_t> sub_knots(knots.begin() + offset, knots.begin() + offset + 2 * degree);
+
+    return bspline_base<data_t>(sub_control_points, sub_knots, t);
   }
 }
 
@@ -63,80 +73,164 @@ namespace ggo
 #include <kernel/ggo_vec2.h>
 
 ////////////////////////////////////////////////////////////////////
-GGO_TEST(bspline, linear)
+GGO_TEST(bspline, base_linear)
 {
   const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f } });
   const std::vector<float> knots({ 0.f, 1.f });
 
-  const ggo::pos2_f p0 = ggo::bspline<ggo::pos2_f>(points, knots, 0.0f);
+  const ggo::pos2_f p0 = ggo::bspline_base<ggo::pos2_f>(points, knots, 0.0f);
   GGO_CHECK_FLOAT_EQ(p0.x(), 0.0f);
   GGO_CHECK_FLOAT_EQ(p0.y(), 1.0f);
 
-  const ggo::pos2_f p1 = ggo::bspline<ggo::pos2_f>(points, knots, 0.5f);
+  const ggo::pos2_f p1 = ggo::bspline_base<ggo::pos2_f>(points, knots, 0.5f);
   GGO_CHECK_FLOAT_EQ(p1.x(), 0.5f);
   GGO_CHECK_FLOAT_EQ(p1.y(), 1.0f);
 
-  const ggo::pos2_f p2 = ggo::bspline<ggo::pos2_f>(points, knots, 1.0f);
+  const ggo::pos2_f p2 = ggo::bspline_base<ggo::pos2_f>(points, knots, 1.0f);
   GGO_CHECK_FLOAT_EQ(p2.x(), 1.0f);
   GGO_CHECK_FLOAT_EQ(p2.y(), 1.0f);
 }
 
 ////////////////////////////////////////////////////////////////////
-GGO_TEST(bspline, quadratic_uniform)
+GGO_TEST(bspline, linear)
+{
+  const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f }, { 1.f, 0.f }, { 2.f, 0.f } });
+  const std::vector<float> knots({ 0.f, 1.f, 2.f, 3.f });
+
+  struct test_case
+  {
+    float _t;
+    ggo::pos2_f _expected_pos;
+  };
+  const std::vector<test_case> test_cases({
+    { 0.0f, { 0.0f, 1.0f } },
+    { 0.5f, { 0.5f, 1.0f } },
+    { 1.0f, { 1.0f, 1.0f } },
+    { 1.5f, { 1.0f, 0.5f } },
+    { 2.0f, { 1.0f, 0.0f } },
+    { 2.5f, { 1.5f, 0.0f } },
+    { 3.0f, { 2.0f, 0.0f } }
+    });
+
+  for (const auto & test_case : test_cases)
+  {
+    const ggo::pos2_f p = ggo::bspline<ggo::pos2_f>(1, points, knots, test_case._t);
+    GGO_CHECK_FLOAT_EQ(p.x(), test_case._expected_pos.x());
+    GGO_CHECK_FLOAT_EQ(p.y(), test_case._expected_pos.y());
+  }
+}
+
+////////////////////////////////////////////////////////////////////
+GGO_TEST(bspline, base_quadratic_uniform)
 {
   const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f }, { 1.f, 0.f } });
   const std::vector<float> knots({ 0.f, 1.f, 2.f, 3.f });
 
-  const ggo::pos2_f p0 = ggo::bspline<ggo::pos2_f>(points, knots, 1.0f);
+  const ggo::pos2_f p0 = ggo::bspline_base<ggo::pos2_f>(points, knots, 1.0f);
   GGO_CHECK_FLOAT_EQ(p0.x(), 0.5f);
   GGO_CHECK_FLOAT_EQ(p0.y(), 1.0f);
 
-  const ggo::pos2_f p1 = ggo::bspline<ggo::pos2_f>(points, knots, 1.5f);
+  const ggo::pos2_f p1 = ggo::bspline_base<ggo::pos2_f>(points, knots, 1.5f);
   GGO_CHECK_FLOAT_EQ(p1.x(), 0.875f);
   GGO_CHECK_FLOAT_EQ(p1.y(), 0.875f);
 
-  const ggo::pos2_f p2 = ggo::bspline<ggo::pos2_f>(points, knots, 2.0f);
+  const ggo::pos2_f p2 = ggo::bspline_base<ggo::pos2_f>(points, knots, 2.0f);
   GGO_CHECK_FLOAT_EQ(p2.x(), 1.0f);
   GGO_CHECK_FLOAT_EQ(p2.y(), 0.5f);
 }
 
 ////////////////////////////////////////////////////////////////////
-GGO_TEST(bspline, quadratic_clamped)
+GGO_TEST(bspline, base_quadratic_clamped)
 {
   const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f }, { 1.f, 0.f } });
   const std::vector<float> knots({ 0.f, 0.f, 1.f, 1.f });
 
-  const ggo::pos2_f p0 = ggo::bspline<ggo::pos2_f>(points, knots, 0.0f);
+  const ggo::pos2_f p0 = ggo::bspline_base<ggo::pos2_f>(points, knots, 0.0f);
   GGO_CHECK_FLOAT_EQ(p0.x(), 0.0f);
   GGO_CHECK_FLOAT_EQ(p0.y(), 1.0f);
 
-  const ggo::pos2_f p1 = ggo::bspline<ggo::pos2_f>(points, knots, 0.5f);
+  const ggo::pos2_f p1 = ggo::bspline_base<ggo::pos2_f>(points, knots, 0.5f);
   GGO_CHECK_FLOAT_EQ(p1.x(), 0.75f);
   GGO_CHECK_FLOAT_EQ(p1.y(), 0.75f);
 
-  const ggo::pos2_f p2 = ggo::bspline<ggo::pos2_f>(points, knots, 1.0f);
+  const ggo::pos2_f p2 = ggo::bspline_base<ggo::pos2_f>(points, knots, 1.0f);
   GGO_CHECK_FLOAT_EQ(p2.x(), 1.0f);
   GGO_CHECK_FLOAT_EQ(p2.y(), 0.0f);
 }
 
 ////////////////////////////////////////////////////////////////////
-GGO_TEST(bspline, cubic_clamped)
+GGO_TEST(bspline, quadratic)
+{
+  const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f }, { 1.f, 0.f }, { 2.f, 0.f } });
+  const std::vector<float> knots({ 0.f, 1.f, 2.f, 3.f, 4.f });
+
+  struct test_case
+  {
+    float _t;
+    ggo::pos2_f _expected_pos;
+  };
+  const std::vector<test_case> test_cases({
+    { 1.0f, { 0.5f, 1.0f } },
+    { 1.5f, { 0.875f, 0.875f } },
+    { 2.0f, { 1.0f, 0.5f } },
+    { 2.5f, { 1.125f, 0.125f } },
+    { 3.0f, { 1.5f, 0.0f } }
+    });
+
+  for (const auto& test_case : test_cases)
+  {
+    const ggo::pos2_f p = ggo::bspline<ggo::pos2_f>(2, points, knots, test_case._t);
+    GGO_CHECK_FLOAT_EQ(p.x(), test_case._expected_pos.x());
+    GGO_CHECK_FLOAT_EQ(p.y(), test_case._expected_pos.y());
+  }
+}
+
+////////////////////////////////////////////////////////////////////
+GGO_TEST(bspline, base_cubic_clamped)
 {
   const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f }, { 1.f, 0.f }, { 2.f, 0.f } });
   const std::vector<float> knots({ 0.f, 0.f, 0.f, 1.f, 1.f, 1.f });
 
-  const ggo::pos2_f p0 = ggo::bspline<ggo::pos2_f>(points, knots, 0.0f);
+  const ggo::pos2_f p0 = ggo::bspline_base<ggo::pos2_f>(points, knots, 0.0f);
   GGO_CHECK_FLOAT_EQ(p0.x(), 0.0f);
   GGO_CHECK_FLOAT_EQ(p0.y(), 1.0f);
 
-  const ggo::pos2_f p1 = ggo::bspline<ggo::pos2_f>(points, knots, 0.5f);
+  const ggo::pos2_f p1 = ggo::bspline_base<ggo::pos2_f>(points, knots, 0.5f);
   GGO_CHECK_FLOAT_EQ(p1.x(), 1.0f);
   GGO_CHECK_FLOAT_EQ(p1.y(), 0.5f);
 
-  const ggo::pos2_f p2 = ggo::bspline<ggo::pos2_f>(points, knots, 1.0f);
+  const ggo::pos2_f p2 = ggo::bspline_base<ggo::pos2_f>(points, knots, 1.0f);
   GGO_CHECK_FLOAT_EQ(p2.x(), 2.0f);
   GGO_CHECK_FLOAT_EQ(p2.y(), 0.0f);
 }
+
+////////////////////////////////////////////////////////////////////
+GGO_TEST(bspline, cubic)
+{
+  const std::vector<ggo::pos2_f> points({ { 0.f, 1.f }, { 1.f, 1.f }, { 1.f, 0.f }, { 2.f, 0.f }, { 2.f, 1.f } });
+  const std::vector<float> knots({ 0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f });
+
+  struct test_case
+  {
+    float _t;
+    ggo::pos2_f _expected_pos;
+  };
+  const std::vector<test_case> test_cases({
+    { 2.0f, { 0.833333f, 0.833333f } },
+    { 2.5f, { 1.0f, 0.5f } },
+    { 3.0f, { 1.166666f, 0.166666f } },
+    { 3.5f, { 1.5f, 0.041666f } },
+    { 4.0f, { 1.833333f, 0.166666f } }
+    });
+
+  for (const auto& test_case : test_cases)
+  {
+    const ggo::pos2_f p = ggo::bspline<ggo::pos2_f>(3, points, knots, test_case._t);
+    GGO_CHECK_FLOAT_EQ(p.x(), test_case._expected_pos.x());
+    GGO_CHECK_FLOAT_EQ(p.y(), test_case._expected_pos.y());
+  }
+}
+
 
 
 
