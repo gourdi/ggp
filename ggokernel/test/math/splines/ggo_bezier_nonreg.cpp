33dcb59ed0355d9db5bef1d2566813e6783c2dc3
Guillaume Gourdin <ggourdin@dxo.com>
2020-07-01 18:02:33 +0200

    Shortened tests.

--- ggokernel/test/math/splines/ggo_bezier_nonreg.cpp
+++ ggokernel/test/math/splines/ggo_bezier_nonreg.cpp
@@ -5,33 +5,17 @@
 ////////////////////////////////////////////////////////////////////
 GGO_TEST(bezier, quadratic)
 {
-  ggo::pos2_f p0 = ggo::quadratic_bezier(ggo::pos2_f(0, 0), ggo::pos2_f(0, 1), ggo::pos2_f(1, 1), 0.f);
-  GGO_CHECK_FLOAT_EQ(p0.x(), 0.f);
-  GGO_CHECK_FLOAT_EQ(p0.y(), 0.f);
-
-  ggo::pos2_f p1 = ggo::quadratic_bezier(ggo::pos2_f(0, 0), ggo::pos2_f(0, 1), ggo::pos2_f(1, 1), 1.f);
-  GGO_CHECK_FLOAT_EQ(p1.x(), 1.f);
-  GGO_CHECK_FLOAT_EQ(p1.y(), 1.f);
-
-  ggo::pos2_f p2 = ggo::quadratic_bezier(ggo::pos2_f(0, 0), ggo::pos2_f(0, 1), ggo::pos2_f(1, 1), 0.5f);
-  GGO_CHECK_FLOAT_EQ(p2.x(), 0.25f);
-  GGO_CHECK_FLOAT_EQ(p2.y(), 0.75f);
+  GGO_CHECK_VEC2F_EQ(ggo::quadratic_bezier<ggo::pos2_f>({ 0, 0 }, { 0, 1 }, { 1, 1 }, 0.0f), ggo::pos2_f(0.f, 0.f));
+  GGO_CHECK_VEC2F_EQ(ggo::quadratic_bezier<ggo::pos2_f>({ 0, 0 }, { 0, 1 }, { 1, 1 }, 1.0f), ggo::pos2_f(1.f, 1.f));
+  GGO_CHECK_VEC2F_EQ(ggo::quadratic_bezier<ggo::pos2_f>({ 0, 0 }, { 0, 1 }, { 1, 1 }, 0.5f), ggo::pos2_f(0.25f, 0.75f));
 }
 
 ////////////////////////////////////////////////////////////////////
 GGO_TEST(bezier, cubic)
 {
-  ggo::pos2_f p0 = ggo::cubic_bezier(ggo::pos2_f(0, 0), ggo::pos2_f(0, 1), ggo::pos2_f(1, 1), ggo::pos2_f(1, 0), 0.f);
-  GGO_CHECK_FLOAT_EQ(p0.x(), 0.f);
-  GGO_CHECK_FLOAT_EQ(p0.y(), 0.f);
-
-  ggo::pos2_f p1 = ggo::cubic_bezier(ggo::pos2_f(0, 0), ggo::pos2_f(0, 1), ggo::pos2_f(1, 1), ggo::pos2_f(1, 0), 1.f);
-  GGO_CHECK_FLOAT_EQ(p1.x(), 1.f);
-  GGO_CHECK_FLOAT_EQ(p1.y(), 0.f);
-
-  ggo::pos2_f p2 = ggo::cubic_bezier(ggo::pos2_f(0, 0), ggo::pos2_f(0, 1), ggo::pos2_f(1, 1), ggo::pos2_f(1, 0), 0.5f);
-  GGO_CHECK_FLOAT_EQ(p2.x(), 0.5f);
-  GGO_CHECK_FLOAT_EQ(p2.y(), 0.75f);
+  GGO_CHECK_VEC2F_EQ(ggo::cubic_bezier<ggo::pos2_f>({ 0, 0 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, 0.0f), ggo::pos2_f(0.f, 0.f));
+  GGO_CHECK_VEC2F_EQ(ggo::cubic_bezier<ggo::pos2_f>({ 0, 0 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, 1.0f), ggo::pos2_f(1.f, 0.f));
+  GGO_CHECK_VEC2F_EQ(ggo::cubic_bezier<ggo::pos2_f>({ 0, 0 }, { 0, 1 }, { 1, 1 }, { 1, 0 }, 0.5f), ggo::pos2_f(0.5f, 0.75f));
 }
 
 
